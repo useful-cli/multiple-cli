a54ba782dca6bb75a66aee2f16e24695b29f6f6d
ZZR-china <2467254599@qq.com>
2017-03-17 13:40:08 +0800

    as

--- gulp/tasks/copy-index.js
+++ gulp/tasks/copy-index.js
@@ -3,8 +3,8 @@ var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins();
 var config = require('../../config').html;
 // process JS files and return the stream.
-gulp.task('copy:htmls', function () {
-    return gulp.src(config.src)
-        .pipe(gulp.dest(config.view))
-        .pipe(plugins.notify({ message: 'HTML task complete' }));
+gulp.task('copy:index', function () {
+    return gulp.src(config.index)
+        .pipe(gulp.dest(config.dist))
+        .pipe(plugins.notify({ message: 'Index task complete' }));
 });
